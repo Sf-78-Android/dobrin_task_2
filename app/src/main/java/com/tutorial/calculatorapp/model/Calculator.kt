831ee094d71c32f6191d49e5243c76fd8802c9f3
dobrin_r <rashkov09@gmail.com>
2023-02-02 19:27:53 +0200

    Minor refactoring on onEqual method to remove if elses

--- app/src/main/java/com/tutorial/calculatorapp/model/Calculator.kt
+++ app/src/main/java/com/tutorial/calculatorapp/model/Calculator.kt
@@ -1,7 +1,15 @@
 package com.tutorial.calculatorapp.model
 
 import com.tutorial.calculatorapp.`interface`.ICalculator
-import com.tutorial.calculatorapp.constants.Constants
+import com.tutorial.calculatorapp.constants.Constants.DIVIDER
+import com.tutorial.calculatorapp.constants.Constants.DOT_ZERO
+import com.tutorial.calculatorapp.constants.Constants.FACTORIAL_SIGN
+import com.tutorial.calculatorapp.constants.Constants.INITIAL_VALUE
+import com.tutorial.calculatorapp.constants.Constants.MINUS
+import com.tutorial.calculatorapp.constants.Constants.MULTIPLIER
+import com.tutorial.calculatorapp.constants.Constants.PLUS
+import com.tutorial.calculatorapp.constants.Constants.REGEX_VALUE
+import com.tutorial.calculatorapp.constants.Constants.SQRT_SIGN
 import kotlin.math.floor
 import kotlin.math.sqrt
 
@@ -11,32 +19,35 @@ class Calculator : ICalculator {
 
     override fun onEqual(input: String): Double {
         val result: Double
-        val regex = Constants.REGEX_VALUE.toRegex()
+        val regex = REGEX_VALUE.toRegex()
         var operations = input.split(regex).toMutableList()
         operations.removeAll(listOf("", null))
-        if (operations[0] == Constants.MINUS) {
+        if (operations[0] == MINUS) {
             operations.removeFirst()
             isNegative = true
         }
-        if (operations[operations.size - 1] == Constants.MINUS || operations[operations.size - 1] == Constants.PLUS ||
-            operations[operations.size - 1] == Constants.MULTIPLIER || operations[operations.size - 1] == Constants.DIVIDER
+        if (operations[operations.size - 1] == MINUS || operations[operations.size - 1] == PLUS ||
+            operations[operations.size - 1] == MULTIPLIER || operations[operations.size - 1] == DIVIDER
         ) {
             operations.removeLast()
         }
         while (operations.size > 1) {
-            if (operations.contains(Constants.SQRT_SIGN)) {
-                operations = calculate(operations, Constants.SQRT_SIGN)
-            } else if (operations.contains(Constants.FACTORIAL_SIGN)) {
-                operations = calculate(operations, Constants.FACTORIAL_SIGN)
-            } else if (operations.contains(Constants.DIVIDER)) {
-                operations = calculate(operations, Constants.DIVIDER)
-            } else if (operations.contains(Constants.MULTIPLIER)) {
-                operations = calculate(operations, Constants.MULTIPLIER)
-            } else if (operations.contains(Constants.MINUS)) {
-                operations = calculate(operations, Constants.MINUS)
-            } else if (operations.contains(Constants.PLUS)) {
-                operations = calculate(operations, Constants.PLUS)
-            }
+            operations =
+                when {
+                    SQRT_SIGN in operations ->
+                        calculate(operations, SQRT_SIGN)
+                    FACTORIAL_SIGN in operations ->
+                        calculate(operations, FACTORIAL_SIGN)
+                    DIVIDER in operations ->
+                        calculate(operations, DIVIDER)
+                    MULTIPLIER in operations ->
+                        calculate(operations, MULTIPLIER)
+                    MINUS in operations ->
+                        calculate(operations, MINUS)
+                    PLUS in operations ->
+                        calculate(operations, PLUS)
+                    else -> mutableListOf()
+                }
         }
 
         result = operations[0].toDouble()
@@ -49,38 +60,38 @@ class Calculator : ICalculator {
     }
 
     override fun calculate(operations: MutableList<String>, operand: String): MutableList<String> {
-        var leftNum: Double = Constants.INITIAL_VALUE
-        var rightNum: Double = Constants.INITIAL_VALUE
+        var leftNum: Double = INITIAL_VALUE
+        var rightNum: Double = INITIAL_VALUE
         while (operations.contains(operand)) {
             val operandIndex = operations.indexOfFirst { it == operand }
-            if (operand != Constants.FACTORIAL_SIGN && operand != Constants.SQRT_SIGN) {
+            if (operand != FACTORIAL_SIGN && operand != SQRT_SIGN) {
                 leftNum = getLeftNum(operations[operandIndex - 1])
                 rightNum = operations[operandIndex + 1].toDouble()
             }
 
             when (operand) {
-                Constants.SQRT_SIGN -> operations[operandIndex] =
+                SQRT_SIGN -> operations[operandIndex] =
                     calculateSqrt(operations[operandIndex + 1]).also {
                         operations.removeAt(
                             operandIndex + 1
                         )
                     }
-                Constants.FACTORIAL_SIGN -> operations[operandIndex - 1] =
+                FACTORIAL_SIGN -> operations[operandIndex - 1] =
                     getFactorial(
                         floor(operations[operandIndex - 1].toDouble()),
                         operations[operandIndex - 1].toDouble()
                     ).also { operations.removeAt(operandIndex) }
-                Constants.DIVIDER -> operations[operandIndex - 1] =
+                DIVIDER -> operations[operandIndex - 1] =
                     (leftNum / rightNum).toString().also {
                         operations.removeAt(operandIndex)
                             .also { operations.removeAt(operandIndex - 1) }
                     }
-                Constants.MULTIPLIER -> operations[operandIndex - 1] =
+                MULTIPLIER -> operations[operandIndex - 1] =
                     (leftNum * rightNum).toString().also {
                         operations.removeAt(operandIndex)
                             .also { operations.removeAt(operandIndex) }
                     }
-                Constants.PLUS -> operations[operandIndex - 1] =
+                PLUS -> operations[operandIndex - 1] =
                     (leftNum + rightNum).toString().also {
                         operations.removeAt(operandIndex)
                             .also { operations.removeAt(operandIndex) }
@@ -96,7 +107,6 @@ class Calculator : ICalculator {
 
     private fun getFactorial(value: Double, total: Double): String {
         var currResult = floor(total)
-
         return if (value <= 1) {
             floor(total).toString()
         } else {
@@ -107,7 +117,7 @@ class Calculator : ICalculator {
 
     override fun removeZeroAfterDot(result: String): Double {
         var value = result
-        if (result.contains(Constants.DOT_ZERO)) {
+        if (result.contains(DOT_ZERO)) {
             value = result.substring(0, result.length - 2)
         }
         return value.toDouble()
@@ -115,7 +125,7 @@ class Calculator : ICalculator {
 
     override fun getLeftNum(number: String): Double {
         if (isNegative) {
-            return Constants.INITIAL_VALUE - number.toDouble()
+            return INITIAL_VALUE - number.toDouble()
         }
         return number.toDouble()
     }
